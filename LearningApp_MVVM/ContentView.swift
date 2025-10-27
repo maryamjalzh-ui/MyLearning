4f8b4e980f409013579c4aecd1b7d87161eabd2e
Maryam <maryamjalzh@gmail.com>
2025-10-27 22:44:51 +0300

    Initial Commit

---
+++ LearningApp_MVVM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearningApp_MVVM
+//
+//  Created by Maryam Jalal Alzahrani on 05/05/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearningApp_MVVM/LearningApp_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp_MVVMApp.swift
+//  LearningApp_MVVM
+//
+//  Created by Maryam Jalal Alzahrani on 05/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
